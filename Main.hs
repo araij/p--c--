18e39048682d5b9a15763ddaa2c728de6a4b3d74
ARAI Junya <arai.junya@lab.ntt.co.jp>
2013-12-22 23:36:55 +0900

    support command-line option

--- Main.hs
+++ Main.hs
@@ -1,19 +1,50 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
-import System.Environment (getArgs)
+import Data.Maybe (fromMaybe)
+import System.Console.CmdArgs
+import System.FilePath (takeBaseName)
 import Dot (digraphToStr)
-import UpcToDot
-import P_C
-import P_CToUpc
+import UpcToDot (upcToDigraph, upcPatentConfig, upcConfig0)
+import P_C (parseP_C)
+import P_CToUpc (p_CToUpc)
+
+data Option = Option { opPatent  :: Bool
+		     , opOutFile :: Maybe String
+                     , opInFile  :: String }
+  deriving (Show, Data, Typeable)
+
+option :: Option
+option = Option { opPatent = False
+		    &= help "Patent mode: add step # to nodes"
+		    &= explicit 
+		    &= name "patent"
+		    &= name "p"
+		, opOutFile = Nothing
+		    &= help "Output file name"
+		    &= explicit
+		    &= name "out"
+		    &= name "o"
+		    &= typFile
+		, opInFile = def
+		    &= argPos 0
+		    &= typ "INPUT_FILE" }
+	  &= program "p-c"
+	  &= summary "P--C-- Compiler, version 0.1.0"
 
 main :: IO ()
 main = do
-  [file] <- getArgs
-  src    <- readFile file
-  putStr $ digraphToStr
-         $ upcToDigraph upcPatentConfig
-         $ p_CToUpc
-         $ fromRight $ parseP_C src
+  op <- cmdArgs option
+  let infile  = opInFile op
+  let outfile = fromMaybe (takeBaseName infile ++ ".dot") $ opOutFile op
+  let upcCfg  = if opPatent op then upcPatentConfig else upcConfig0
+  src <- readFile infile
+  writeFile outfile $ digraphToStr
+		    $ upcToDigraph upcCfg
+		    $ p_CToUpc
+		    $ fromRight
+		    $ parseP_C src
   where
     fromRight (Right x) = x
     fromRight (Left x)  = error $ "fromRight: Left is given: " ++ show x
--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-EXECS   = p--c-- Test
+EXECS   = p-c Test
 HSFLAGS = -Wall
 SRCS    = $(wildcard *.hs)
 
 all: $(EXECS)
 
 .PHONY: $(EXECS)
-p--c--:
+p-c:
 	ghc $(HSFLAGS) --make Main -o $@
 Test:
 	ghc $(HSFLAGS) --make $@
